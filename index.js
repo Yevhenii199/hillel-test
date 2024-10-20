26ae436fefa907f073d1232bbb7f858d5cae2748
Yevhenii199 <gshijanov5@gmail.com>
2024-10-20 13:20:32 +0200

    Added file index.js , done 3.1

---
+++ index.js
@@ -0,0 +1,22 @@
+//3.1
+
+//primitive data types
+console.log(typeof 1);
+
+console.log(typeof "Hello , my friend!");
+
+console.log(typeof true);
+
+let age = null;
+console.log(age);
+
+console.log(typeof undeclaredVariable);
+
+console.log(typeof 1n);
+
+console.log(typeof Symbol() )
+
+//object data types
+console.log(typeof {});
+
+console.log(typeof function(){});
