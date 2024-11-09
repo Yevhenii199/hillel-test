6e2c18225c1b389a2e6e69d3fb684b8ef66b2bdf
Yevhenii199 <gshijanov5@gmail.com>
2024-11-09 10:06:45 +0100

    Added 8.1

--- index.js
+++ index.js
@@ -1,46 +1,22 @@
-//7.1
-function createSum() {
-  let total = 0; 
-  
-  return function(num) {
-    total += num; 
-    return total; 
-  };
-}
-const sum = createSum();
+//8.1
+let ladder = {
+  step: 0, 
 
-console.log(sum(4));  
-console.log(sum(6));  
-console.log(sum(10)); 
+  up: function () {
+    this.step++;
+    return this; 
+  },
 
-//7.2
-function multiply(a) {
-  return function(b) {
-    return a * b;
-  };
-}
-const result = multiply(5)(2); 
+  down: function () {
+    this.step--;
+    return this; 
+  },
 
-//7.3
-function requestNumber() {
-  let input;
-  
-  for (let i = 0; i < 10; i++) {
-    input = prompt("Введіть число більше 100:");
-    
-    if (input === null) {
-      console.log("Користувач відмінив ввод.");
-      return; 
-    }
-    
-    input = Number(input); 
-    if (input > 100) {
-      console.log("Останнє дійсне введення:", input);
-      return; 
-    }
+  showStep: function () {
+    console.log(this.step); 
+    return this;
   }
-  
-  console.log("Останнє введення після 10 спроб:", input); 
-}
+};
 
-requestNumber();
+ladder.up().up().down().showStep(); 
+ladder.up().up().down().showStep();
