9d8e92facbaeecf4b3010d2ff0b8626da71385c3
Yevhenii199 <gshijanov5@gmail.com>
2024-11-13 19:54:32 +0100

    Added 10.2

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ let inf = {
 }
 console.log(inf.getInfo());
 
+//10.2
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+let evenNumbers = numbers.filter(num => num % 2 === 0);
+
+console.log(evenNumbers);
 
 
 
